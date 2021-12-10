2f81b1f9b56280b0a8134c2c283fe59d32ac3f7a
Makeda Davis <1makedadavis@gmail.com>
2021-12-10 02:34:36 -0500

    created global variables

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,6 +1,11 @@
 const {Pool} = require('pg');
+require('dotenv').config();
 ;
-const connectionString =
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const port = process.env.PORT;
+const db = process.env.DB
+const connectionString =`postgresql://postgres:${password}@${host}:${port}/${db}`
 
 const pool = new Pool ({
 
@@ -8,6 +13,6 @@ const pool = new Pool ({
 })
 
 pool.connect().then(res => console.log("connected"))
-.catch(err => console.log(err))
+.catch(err => console.log(err, password, host, port, db))
 
 module.exports = pool;

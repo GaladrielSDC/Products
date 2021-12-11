a80eba2ed3e94e58c3bfb3e78830e3b9518628a1
Makeda Davis <1makedadavis@gmail.com>
2021-12-11 14:54:08 -0500

    implemented styles query with build obj

--- Server/index.js
+++ Server/index.js
@@ -45,17 +45,77 @@ app.get('/products/:product_id', (req, res) => {
   })
 })
 
+
 app.get('/products/:product_id/styles', (req, res) => {
-  const productId = req.params.product_id;
-  pool.query(`select * from style where product_id = ${productId} `,(err, results) => {
-    if (err) {
-      throw err
-    }
-    res.status(200).json(results.rows)
+//   const productId = req.params.product_id;
+//   pool.query(`select * from style where product_id = ${productId} `,(err, results) => {
+//     if (err) {
+//       throw err//     }
+//     res.status(200).json(results.rows)
+// })
+const stylesQuery = `SELECT
+product_id,
+json_build_object('results',
+          json_agg(json_build_object(
+            'style_id', id,
+            'name', style_name,
+            'original_price', original_price,
+            'sale_price', sales_price,
+            'default?', default_style,
+            'photos', (
+              SELECT json_agg(
+                json_build_object(
+                  'thumbnail_url', thumbnail_url,
+                  'url', photo_url
+                )
+              )
+              FROM photo
+              WHERE photo.style_id = style.id
+              GROUP BY style.id
+            ),
+            'skus', (
+              SELECT json_object_agg(sku.id, json_build_object(
+                'quantity', quantity,
+                'size', size
+              )
+              )
+              FROM sku
+              WHERE sku.style_id = style.id
+              GROUP BY style.id
+            )
+           )
+           )
+         )
+FROM
+style
+WHERE
+product_id = $1
+GROUP BY
+product_id
+ORDER BY
+product_id
+`
+
+pool.query(stylesQuery, [req.params.product_id], (err,results) => {
+  if (err) {
+    res.status(500)
+  }
+  // console.log(results)
+  if (results.rows.length === 0) {
+    res.json({
+      product_id: req.params.id,
+      results: []
+    })
+  } else {
+
+  return res.json({
+    product_id: results.rows[0].product_id,
+    results: results.rows[0].json_build_object.results
+  })
+}
+})
 })
 
-
-})
  app.get('/products/:product_id/related', (req, res) => {
    const productId = req.params.product_id;
    console.log(pool)
